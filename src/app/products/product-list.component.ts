3c32e9478908b7d9d904f3b378c93c30cd14b792
AleksandraOdr <aleksandra.odrobinska@gmail.com>
2020-01-09 14:43:32 +0100

    front page

---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './product-list.component.html'
+})
+
+export class ProductListComponent {
+
+}
