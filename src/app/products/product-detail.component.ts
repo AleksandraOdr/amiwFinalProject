36a11fdf3e136a3e7c44356adf2d1ec805e702e6
AleksandraOdr <aleksandra.odrobinska@gmail.com>
2020-02-29 18:20:43 +0100

    Routing + guard

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Route, Router} from '@angular/router';
 
 import {IProduct} from './product';
 
@@ -10,10 +11,28 @@ export class ProductDetailComponent implements OnInit {
   pageTitle = 'Opis produktu';
   product: IProduct;
 
-  constructor() {
+  constructor(private route: ActivatedRoute,
+              private router: Router) {
   }
 
   ngOnInit() {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id}`;
+    this.product = {
+      productId: id,
+      productName: 'Pad XBOX ONE',
+      productCode: 'GMG-0042',
+      releaseDate: '15 Marca 2020',
+      // tslint:disable-next-line:max-line-length
+      description: 'Przeznaczenie: Xbox One, Xbox One S, Xbox One X.\nInterfejs Bezprzewodowy.\nZastosowane technologie Wi-Fi.\nZmienia kolor w zależności od kąta patrzenia z niebieskiego na fioletowy.\nZasilanie: 2 paluszki AA.',
+      price: 199.99,
+      starRating: 4.6,
+      imageUrl: 'assets/images/xbox-controller.jpg'
+    };
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
   }
 
 }
